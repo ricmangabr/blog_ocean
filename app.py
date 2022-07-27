93ad413ba42d4356d16ab7804fd6d57d97df2288
Ricardo K M Albernaz <ricmanga@gmail.com>
2022-07-27 00:33:35 +0000

    Fim da aula 3

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
-
+from datetime import datetime
 app = Flask("hello")
 
+posts = [
+    {
+        "title": "O meu primeiro post", 
+        "body": "Aqui é o texto do post",
+        "author": "Feulo",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu segundo post", 
+        "body": "Aqui é o texto do post",
+        "author": "Danilo",
+        "created": datetime(2022,7,26)
+    },
+]
+# é uma lista de dicionários dentro
 @app.route("/")  # cria uma rota na raiz
+def index():
+    return render_template("index.html", posts = posts)
 
-def hello(): #cria
-    return "Hello world!"
 
-@app.route("/meucontato") #outra rota para contato
-def meuContato():
-    return render_template('index.html') 
-    
